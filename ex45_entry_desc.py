6a251a90d2fc699f2fca73c99f0b014ef4572464
Tara Habersack <taraleehabersack@gmail.com>
2015-01-07 20:54:27 -0800

    polishing text

--- ex45_entry_desc.py
+++ ex45_entry_desc.py
@@ -21,43 +21,49 @@ evil_landlord = """You enter a modest one level apartment.
 
 ghetto = """You had noticed on the map that the apartment was
 	 close to the freeway; now you realize that the back
-	 fence is the freeway cinderblock. The leasing agent 
+	 fence is the freeways' cinderblock wall. The leasing agent 
 	 gets out of his car as you walk up. His cheesy 
-	 car-salesman smile on his face and the mustard on 
-	 his tie are adequately amusing. "This place is a steal! 
-	 If you're looking to save money, then this is it! First
+	 car-salesman smile and the mustard on his tie are 
+	 adequately amusing. "This place is a steal," he shouts. 
+	 "If you're looking to save money, then this is it! First
 	 I want you to notice the windows, take a real good look.
-	 Wow! That's security! And peice of mind- nothings getting 
+	 Wow! That's security! And peice of mind- nothing's getting 
 	 through there! As close as you are to the freeway you 
 	 don't have to wait in line just to get to work. The 
-	 onramp is right around the corner! The apartment is 
-	 apparently the lower level of the buidling that you 
-	 arrived at. The agent takes you down the outer basement 
-	 steps and inside. Richly colored paint, step in shower, 
-	 gold colored faucets and a laundry hookup in the bathroom. 
-	 Someone sunk some money into this basement. The agent is 
-	 sure to point out every detail and only gives you two 
-	 minutes to look by yourself before coming back.
-	 	 """
+	 onramp is right around the corner!" The apartment is 
+	 apparently the lower level of the building that you 
+	 arrived at. The agent leads you down the outer basement 
+	 steps and inside. The good points are the richly colored 
+	 paint, the step in shower, the gold colored faucets and a 
+	 laundry hookup in the bathroom. Someone sunk some money 
+	 into this basement. The agent is sure to point out every 
+	 detail and only gives you two minutes to look by yourself 
+	 before coming back.
+	 """
 
 our_new_home = """This neighborhood is quiet and clean. You notice
-	       kids playing in a front yard. Three blocks away in  
-	       one direction is a big grocery store, a block away
-	       in another direction is a market. The leasing agent
-	       arrives when you do. She takes you through. A
-	       stacked washer/dryer combo come with the apartment.
-	       There aren't any bars on the windows. A couple of 
-	       windows look out to trees in neighboring yards.
-               The leasing agent chats you up as she walks you from 
-	       room to room and she mentions that she has two other
-	       veiwing here today.
+	       kids playing in the front yard directly across the 
+	       small lane from the six unit apartment building that
+	       you are considering. You call to mind your research:
+	       Three blocks away in one direction is a supermarket, 
+	       a block away in another direction is a mom n' pop 
+	       market. The leasing agent arrives as you do. She takes 
+	       you through. A stacked washer/dryer combo are included.
+	       There aren't any bars on the windows which lets you
+	       appreciate the ample sunlight streaming through the 
+	       thick wood blinds. A couple of windows look out to 
+	       trees in neighboring yards. The leasing agent chats 
+	       you up as she walks you from room to room. She mentions 
+	       that she has two other veiwings here today but she
+	       gives you your space.
                """
 
 closet =  """In the middle of everything, thats where this apartment is.
           restaurants galore, bars, neighborhood markets, across the
           street from the farmers market. Finding parking takes half
-          an hour but once your in front of the tall, narrow brick 
-          building you can feel the energy. The leasing agent lets
+          an hour but once you're standing in front of the tall, narrow 
+          brick building that contends with historical giants on the 
+          skyline you can feel the energy. The leasing agent lets
           you in and gets back to conducting business on her Ipad while
           you show yourself around. It's 300 square feet. Hard wood 
 	  floors, a shower and a loft sleeping space. Its a little 
@@ -66,7 +72,7 @@ closet =  """In the middle of everything, thats where this apartment is.
 	  with your tour.
 	  """
 
-bunking_with_strangers = """You walk up the steps of a large two story house. 
+bunking_with_strangers = """You walk up the steps of a large, two story house. 
               		 the bottom level is partially underground and appears
               		 to be undergoing construction. A trash bag and a 
               		 recycling bin sit next to the door. An argument is
@@ -78,17 +84,24 @@ part_2 =  """A fresh-faced college dude greets you. He tucks
           apologetic for the argument going on in the next room.
           He whispers as he ushers you down the hall quickly.
           The kitchen is big and not unreasonably messy. Your 
-          room is not tiny. The other roomies are not at home.
-          He offers to hang out with you while you wait for 
-          Caitlyn to get off the phone with her (ex?) boyfriend.
-          Apparently she can finish the tour for you.
+          room might have been a large closet at one time. The 
+          other roomies are not at home. "We have a lot of 
+          people coming to check out the room so I just want
+          you to know so you're not dissappointed or anything
+          that there are a lot of people interested in it. Just
+          so you know." He offers to hang out with you while you 
+          wait for Caitlyn to get off the phone with her (ex?) 
+          boyfriend. Apparently she can finish the tour for you.
           """	
           
 roomie = """When she finishes her argument she ducks into the kitchen
-         and apologizes. \"My boyfriend is kind of an idiot sometimes.\"
+         and apologizes. "My boyfriend is kind of an idiot sometimes."
          You talk for a while and come to the conclusion that the two
          out of four roommates that you are able to meet are nice enough.
          Although a little hot headed and definitely young. She points
          out a good sized back yard and the fact that there is standing
          room only in the eating area as the dining area is now a bedroom.
+         All dishes are washed by hand and they have a fairly complicated
+         recycling and composting system that Caitlyn stresses is Very 
+         important.
          """          
